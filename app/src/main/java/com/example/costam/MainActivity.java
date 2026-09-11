a28fac11aea745659072b3347b6376beacca80c8
avg <avg@cos.pl>
2026-09-11 13:52:04 +0200

    v1.0

--- app/src/main/java/com/example/costam/MainActivity.java
+++ app/src/main/java/com/example/costam/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.costam;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void jakas_metoda(View view) {
+        TextView tekst = findViewById(R.id.przycisk_1);
+        tekst.setText("Kliknięto przycisk 1");
+
+
+
+    }
+    int wartosc = 0;
+    public void dodaj_1(View view) {
+        TextView tekst2 = findViewById(R.id.przycisk_2);
+        wartosc = wartosc+1;
+        tekst2.setText(String.valueOf(wartosc));
+
+    }
+
+    public void odejmowanie(View view) {
+        TextView tekst2 = findViewById(R.id.przycisk_2);
+        wartosc = wartosc-1;
+        tekst2.setText(String.valueOf(wartosc));
+    }
+
+    public void reset(View view) {
+        TextView tekst2 = findViewById(R.id.przycisk_2);
+        wartosc = 0;
+        tekst2.setText(String.valueOf(wartosc));
+
+    }
+
+    public void jakas_metoda2(View view) {
+        TextView tekst = findViewById(R.id.przycisk_1);
+        tekst.setText("Kliknięto przycisk 2");
+
+
+    }
+
+    public void jakas_metoda_reset(View view) {
+        TextView tekst = findViewById(R.id.przycisk_1);
+        tekst.setText("");
+
     }
 }
